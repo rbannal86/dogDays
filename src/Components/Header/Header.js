fd32168462183169c56fdcac585bb03a702fb55c
Alex Bannow <rbannal@gmail.com>
2020-10-27 13:56:36 -0400

    added gradient deco to header and styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,41 @@
 import React from "react";
+import HeaderDeco from "../HeaderDeco/HeaderDeco";
+
+import "./Header.css";
 
 export default function Header(props) {
   return (
     <div className={"header_main"}>
-      <h1>dogDays</h1>
-      {props.userId ? (
-        <button onClick={() => props.handleLogOut()}>Log Out</button>
-      ) : (
-        <>
-          <button onClick={() => props.setView("login")}>Log In</button>
-          <button onClick={() => props.setView("register")}>Register</button>
-        </>
-      )}
+      <HeaderDeco />
+      <div className={"header_content"}>
+        {" "}
+        <h1 className={"header_title"}>dogDays</h1>
+        {props.userId ? (
+          <div className={"header_button_div"}>
+            <button
+              className={"header_button"}
+              onClick={() => props.handleLogOut()}
+            >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <div className={"header_button_div"}>
+            <button
+              className={"header_button"}
+              onClick={() => props.setView("login")}
+            >
+              Log In
+            </button>
+            <button
+              className={"header_button"}
+              onClick={() => props.setView("register")}
+            >
+              Register
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Components/HeaderDeco/HeaderDeco.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./HeaderDeco.css";
+
+export default function HeaderDeco() {
+  return (
+    <div className={"header_deco_main"}>
+      <div className={"header_deco_segment"} id={"deco_1"}></div>
+      <div className={"header_deco_segment"} id={"deco_2"} />
+      <div className={"header_deco_segment"} id={"deco_3"} />
+      <div className={"header_deco_segment"} id={"deco_4"} />
+      <div className={"header_deco_segment"} id={"deco_5"} />
+      <div className={"header_deco_segment"} id={"deco_6"} />
+      <div className={"header_deco_segment"} id={"deco_7"} />
+      <div className={"header_deco_segment"} id={"deco_8"} />
+      <div className={"header_deco_segment"} id={"deco_9"} />
+      <div className={"header_deco_segment"} id={"deco_10"} />
+    </div>
+  );
+}
